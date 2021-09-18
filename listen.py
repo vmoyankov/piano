82fa7b7c95f582e74b075c43fb2db217b6207239
Venko Moyankov <venko@storpool.com>
2021-09-18 15:59:37 +0300

    listen: guess the tones

--- listen.py
+++ listen.py
@@ -8,15 +8,15 @@ import time
 import mido
 
 
-onames = mido.get_output_names()
-fluid = list(filter(lambda x: 'FP-30' in x, onames))
+names = mido.get_output_names()
+piano = list(filter(lambda x: 'FP-30' in x, names))
 
-if len(fluid) == 0:
-    print("Fluid synth not started")
+if len(piano) == 0:
+    print("The piano is not connected")
     sys.exit(1)
 
-fport = mido.open_output(fluid[0])
-print(f'Connected to {fluid[0]}')
+port = mido.open_ioport(piano[0])
+print(f'Connected to {piano[0]}')
 
 
 int_names = [
@@ -35,20 +35,58 @@ int_names = [
         'Октава',     # 12
         ]
 
+Major = [0,2,4,5,7,9,11]
+Minor = [0,2,3,5,7,8,10]
 
-def play_notes(notes, delay=0, duration=1.0, arp=False):
+def play_notes(notes, delay=0, duration=1.0, arp=False, channel=1):
     for note in notes:
-        msg=mido.Message('note_on', note=note)
-        fport.send(msg)
+        msg=mido.Message('note_on', note=note, channel=channel)
+        port.send(msg)
         time.sleep(delay)
         if arp:
-            msg=mido.Message('note_off', note=note)
-            fport.send(msg)
+            msg=mido.Message('note_off', note=note, channel=channel)
+            port.send(msg)
     if not arp:
         time.sleep(duration)
         for note in notes:
-            msg=mido.Message('note_off', note=note)
-            fport.send(msg)
+            msg=mido.Message('note_off', note=note, channel=channel)
+            port.send(msg)
+
+
+def guess_note(choice=Major+[12]):
+    base_n = 60 # C4
+    old = 0
+    k = 0
+    while True:
+        while abs(k - old) < 3:
+            k = random.choice(choice)
+        old = k
+        #o = random.choice((-12, 0 , 12))
+        o = 0
+        n = base_n + o + k
+        print(o, k)
+        play_notes((n,))
+        while True:
+            m = port.receive()
+            if m.type == 'note_on':
+                inote = m.note
+                if inote == 21:
+                    return
+                if inote == 22:
+                    break
+                if inote == 23:
+                    play_notes((n,))
+                    continue
+                if inote == n:
+                    print("OK")
+                    time.sleep(0.2)
+                    play_notes((60,), channel=9, duration=0.3)
+                    break
+                else:
+                    a = inote % 12
+                    print(f"Error:{k} -> {a}")
+
+        time.sleep(2)
 
 
 def play_random_intervals(n=10, choice=list(range(13)), base=None):
@@ -73,10 +111,11 @@ def play_random_intervals(n=10, choice=list(range(13)), base=None):
         time.sleep(1)
 
 
+
+
 def print_intervals():
     for i, n in enumerate(int_names):
         print(i, n)
 
 if __name__ == '__main__':
-    print_intervals()
-    play_random_intervals(100,[3,4])
+    guess_note()
---
+++ piano.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+fluidsynth -a pulseaudio /usr/share/sounds/sf2/FluidR3_GM.sf2 
